da726aa9f8658961f313e880d0e251c7b37b658a
Vinesma <otaviocos_13@hotmail.com>
2019-06-30 18:18:10 -0300

    Add infinite loop protection and icon

--- bot.js
+++ bot.js
@@ -32,33 +32,35 @@ bot.on('ready', function (evt) {
 bot.on('message', (user, userID, channelID, message, evt) =>{
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        let args = message.substring(1).split(' ');
-        let cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd.toLowerCase()) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            case 'youtube':
-                getLatestVideos(channelID);              
-            break;
-            case 'off':
-                bot.disconnect();              
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+    if (userID !== bot.id) {
+        if (message.substring(0, 1) == '!') {
+            let args = message.substring(1).split(' ');
+            let cmd = args[0];
+           
+            args = args.splice(1);
+            switch(cmd.toLowerCase()) {
+                // !ping
+                case 'ping':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Pong!'
+                    });
+                break;
+                case 'youtube':
+                    getLatestVideos(channelID);              
+                break;
+                case 'off':
+                    bot.disconnect();              
+                break;
+                // Just add any case commands if you want to..
+             }
+        }    
+    }  
 });
 
-function getLatestVideos(channelID, nReq) {
+function getLatestVideos(channelID) {
     // nReq > 10 ? nReq = 10 : nReq;
-    nReq = 1;
+    nReq = 3;
 
     fetch(`https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=UCQBs359lwzyVFtc22LzLjuw&maxResults=${nReq}&key=${auth.ytAPIToken}`)
     .then((res) => res.json())
